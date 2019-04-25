e5d62c0a08f28cb43ed784f27d126493142bd946
Javier <javier@insendi.com>
2019-04-25 11:27:01 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+// Define our app...
+const App = () => {
+  // Set the initial value when the app is first constructed.
+  const [value, setValue] = useState(initialValue)
+
+  // On change, update the app's React state with the new editor value.
+  const onChange = ({ value }) => {
+    setValue(value);
+  }
+
+  // Render the editor.
+  return ( 
+    <Editor value={value} onChange={onChange} />
   );
 }
 
